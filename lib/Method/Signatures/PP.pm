f49ef0f687501767f284d2cebc01aa8767b0cc36
Matt S Trout <mst@shadowcat.co.uk>
2017-06-21 18:43:10 +0100

    second checkpoint

--- lib/Method/Signatures/PP.pm
+++ lib/Method/Signatures/PP.pm
@@ -5,10 +5,16 @@ use warnings;
 use Filter::Util::Call;
 use PPR;
 
+our $Statement_Start;
+
+our @Found;
+
 my $grammar = qr{
   (?(DEFINE)
     (?<PerlKeyword>
+      (?{ local $Statement_Start = pos() })
       method(?&PerlOWS)(?&PerlIdentifier)(?&PerlOWS)(?&PerlBlock)(?&PerlOWS)
+      (?{ push @Found, [ $Statement_Start, pos() - $Statement_Start ] })
     )
   )
   $PPR::GRAMMAR
@@ -20,26 +26,30 @@ sub import {
     return 0 if $done++;
     1 while filter_read();
     warn "CODE >>>\n$_<<<";
-    while (/(?&PerlOWS)?((?&PerlStatement)) $grammar/xg) {
-      my $len = length($1);
-      my $pos = pos();
-      my $start = $pos - $len;
-      my $stmt = $1;
-warn "ST >>>\n$stmt<<<";
-      if (my @match = $stmt =~ m{
-        \A
-        method ((?&PerlOWS))
-        ((?&PerlIdentifier)) ((?&PerlOWS))
-        ((?&PerlBlock)) ((?&PerlOWS))
-        $PPR::GRAMMAR
-      }x) {
-        my ($ws0, $name, $ws1, $block, $ws2) = @match;
-        $block =~ s{^\{}{\{my \$self = shift;};
-        my $replace = "sub${ws0}${name}${ws1}${block}${ws2}";
-        substr($_, $start, $len) = $replace;
-        pos() = $pos + length($replace) - $len;
-      }
+    local @Found;
+    die "Um. What?" unless /(?&PerlDocument) $grammar/x;
+    ::Dwarn @Found;
+    my $offset = 0;
+    foreach my $case (@Found) {
+      my ($start, $len) = @$case;
+      $start += $offset;
+      my $stmt = substr($_, $start, $len);
+warn $stmt;
+      die "Whit?"
+        unless my @match = $stmt =~ m{
+          \A
+          method ((?&PerlOWS))
+          ((?&PerlIdentifier)) ((?&PerlOWS))
+          ((?&PerlBlock)) ((?&PerlOWS))
+          $PPR::GRAMMAR
+        }x;
+      my ($ws0, $name, $ws1, $block, $ws2) = @match;
+      $block =~ s{^\{}{\{my \$self = shift;};
+      my $replace = "sub${ws0}${name}${ws1}${block}${ws2}";
+      substr($_, $start, $len) = $replace;
+      $offset += length($replace) - $len;
     }
+    warn "FINAL >>>\n$_<<<";
     return 1;
   });
 }
